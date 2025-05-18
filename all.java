9cda3c643a33de71f4b6735d62b2a4062c80711c
HASHVANTHA <hashvantham@gmail.com>
2025-05-18 11:31:45 +0530

    Add files via upload

---
+++ all.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s = new Scanner(System.in);
+	    System.out.println("Enter a integer:");
+	    int integer = s.nextInt();
+	    s.nextLine();
+	    System.out.println("Enter a string:");
+	    String str = s.nextLine();
+	    System.out.println("Enter a Character:");
+	    char c = s.next().charAt(0);
+        System.out.println(integer);
+        System.out.println(str);
+        System.out.println(c);
+        
+	}
+}
