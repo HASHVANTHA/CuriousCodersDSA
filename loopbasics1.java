893d1ac3deba1d45f4a9ee3adeb9653dd0360e7a
HASHVANTHA <hashvantham@gmail.com>
2025-05-18 14:11:24 +0530

    Add files via upload

---
+++ loopbasics1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+	    int x = s.nextInt();
+	    int y = s.nextInt();
+	    for(int i=0;i<y;i++)
+	    {
+	        System.out.println(x);
+	    }
+	}
+}
---
+++ loopbasics3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		String f = s.nextLine();
+		String str = s.nextLine();
+		int n = s.nextInt();
+		String x = f.concat(str);
+		for(int i=0;i<n;i++)
+		{
+		    System.out.println(x);
+		}
+		
+
+	}
+}
